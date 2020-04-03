7996575f3510f4b89fcc5c581c102768c2774a14
Virtually Nick <vnick@apache.org>
2020-04-02 23:19:43 -0400

    GUACAMOLE-728: Implement MySQL driver SSL configuration.

--- extensions/guacamole-auth-jdbc/modules/guacamole-auth-jdbc-mysql/src/main/java/org/apache/guacamole/auth/mysql/conf/MySQLSSLMode.java
+++ extensions/guacamole-auth-jdbc/modules/guacamole-auth-jdbc-mysql/src/main/java/org/apache/guacamole/auth/mysql/conf/MySQLSSLMode.java
@@ -17,21 +17,26 @@
  * under the License.
  */
 
-package org.apache.guacamole.auth.mysql;
+package org.apache.guacamole.auth.mysql.conf;
 
 /**
- * The possible JDBC drivers to use when talking to a MySQL-compatible database
- * server.
+ * Possible values for enabling SSL within the MySQL Driver.
  */
-public enum MySQLDriver {
-
-    /**
-     * MySQL driver.
-     */
-    MYSQL,
-
-    /**
-     * MariaDB driver.
-     */
-    MARIADB;
+public enum MySQLSSLMode {
+    
+    // Disable SSL altogether.
+    DISABLED,
+    
+    // Prefer SSL, but fall-back to non-SSL.
+    PREFERRED,
+    
+    // Require SSL, but perform no verification.
+    REQUIRED,
+    
+    // Require SSL and verify a valid authority.
+    VERIFY_CA,
+    
+    // Require SSL and verify a valid authority and server certificate.
+    VERIFY_IDENTITY;
+    
 }
